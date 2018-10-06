7fc19f8441b726e23f407e9716b1526cca9ec19b
Cody Cook <codycook33@gmail.com>
2018-10-06 16:49:43 -0700

    trying this again because it refuses to work properly

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+
+public class LoadScene : MonoBehaviour {
+
+	
+	Public Manager manager;	
+	
+	void Start ( )
+	{
+		counter = manager.GetComponent<bossCounter>();
+	}
+	void OnTriggerExit (Collider col)
+	{
+		
+			
+			if (col.gameObject.name == "Player")
+			{
+				Vector3 relativePos = transform.InverseTransformPoint(col.transform.position);
+				if (relativePos.z > 0)
+				{
+					if (counter >= 5)
+					{
+						LoadBossScene();
+					}
+				}
+				else
+				{
+					LoadScene();
+					counter++;
+				}
+				
+			}
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(LevelScene);
+	}		
+	
+	public void LoadBossScene(string bossScene)
+	{
+		SceneManager.LoadScene(BossScene);
+	}	
+		
+
+
+
+	}
