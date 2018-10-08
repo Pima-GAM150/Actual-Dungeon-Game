df6a096dd516cf3fc6a93129fa76fb1236f87750
Drew Castalia <dcastalia@email.arizona.edu>
2018-10-08 12:34:47 -0700

    Testing the ability to commit / push.

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager : MonoBehaviour {
+
+	public float test;
 
 	// Use this for initialization
 	void Start () {
